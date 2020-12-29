6ff6cf07d13b007697c55fde0b808ba9427f88ab
Sharan Aithal <sharanaithal0@yahoo.com>
2020-12-29 22:59:28 +0530

    added dashboard router and ejs view.

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const pool = require("./model/database");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const pool = require("./model/database");
+var dashboardRouter = require('./routes/dashboard');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
