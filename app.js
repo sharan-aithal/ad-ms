5e398b020c89c5d90463aa22c82d4ffd7ced9e03
Sharan Aithal <sharanaithal0@yahoo.com>
2020-12-29 19:07:16 +0530

    changed pg.Client to pg.Pool

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const pool = require("./model/database");
 
 var app = express();
 
@@ -13,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.set(pool.connect())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
