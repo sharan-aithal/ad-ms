1aa3a7291105ac796f411853d74cee011402a313
Sharan Aithal <sharanaithal0@yahoo.com>
2020-12-29 17:38:18 +0530

    introduced database connection.

---
+++ model/database.js
@@ -0,0 +1,11 @@
+var { Client } = require('pg');
+
+const client = new Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DB,
+    password: process.env.PG_PASS,
+    port: process.env.PG_PORT
+    });
+
+module.exports = client;
